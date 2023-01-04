e83e764c09b77a85d723ee091c321a00c6999f00
Mehmet Talha Irmak <mehmettalha.irmak@hotmail.com>
2023-01-04 19:41:52 +0300

    RNFS downloadFile method template created

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,5 +1,11 @@
 import React, { useEffect } from 'react';
-import { SafeAreaView, View, StyleSheet } from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Platform,
+  PermissionsAndroid,
+} from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import PostCard from '../components/PostCard';
 import Button from '../components/Button';
@@ -8,13 +14,51 @@ import {
   widthPercentageToDP as wp,
 } from 'react-native-responsive-screen';
 import ScreenHeader from '../components/ScreenHeader';
+import { CameraRoll } from '@react-native-camera-roll/camera-roll';
+
+var RNFS = require('react-native-fs');
 
 const PostScreen = () => {
   const navigationRoute = useRoute();
 
+  const hasAndroidPermission = async () => {
+    const permission =
+      Platform.Version >= 33
+        ? PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES
+        : PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE;
+
+    const hasPermission = await PermissionsAndroid.check(permission);
+
+    if (hasPermission) {
+      return true;
+    }
+
+    const status = await PermissionsAndroid.request(permission);
+    return status === 'granted';
+  };
+
   useEffect(() => {
     //console.log(navigationRoute?.params?.item);
   }, [navigationRoute]);
+
+  const saveImages = async () => {
+    const fileState = await hasAndroidPermission();
+    if (Platform.OS === 'android' && !(await hasAndroidPermission())) {
+      return;
+    }
+
+    RNFS.downloadFile({
+      fromUrl: '',
+      toFile: '',
+      cacheable,
+      progressInterval: 100,
+      begin: () => {
+        console.log('download start');
+      },
+    }).promise.then(() => {
+      console.log('download finished');
+    });
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -29,7 +73,7 @@ const PostScreen = () => {
           status="save"
           textColor="white"
           text="Save"
-          onPress={() => {}}
+          onPress={saveImages}
         />
       </View>
     </SafeAreaView>
