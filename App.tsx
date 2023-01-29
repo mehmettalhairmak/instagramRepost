e5be320b83e1bb62a397de8089f0128ce8a47226
Mehmet Talha Irmak <mehmettalha.irmak@hotmail.com>
2023-01-29 23:34:34 +0300

    Added Places and Place Details Screen for Apple and Meta Reviews.

--- App.tsx
+++ App.tsx
@@ -5,12 +5,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import PostScreen from './src/screens/PostScreen';
+import PlaceListScreen from './src/screens/PlaceListScreen';
 import AuthContextProvider from './src/context/AuthContextProvider';
+import PlaceDetailScreen from './src/screens/PlaceDetailScreen';
+import { isDebug } from './src/constants/general';
 
 export type RootStackParams = {
   LoginScreen: any;
   HomeScreen: any;
   PostScreen: any;
+  PlaceListScreen: any;
+  PlaceDetailScreen: any;
 };
 
 const Stack = createNativeStackNavigator<RootStackParams>();
@@ -23,8 +28,23 @@ const App = () => {
           initialRouteName="LoginScreen"
           screenOptions={{ headerShown: false }}>
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
-          <Stack.Screen name="PostScreen" component={PostScreen} />
+          {isDebug !== true ? (
+            <>
+              <Stack.Screen name="HomeScreen" component={HomeScreen} />
+              <Stack.Screen name="PostScreen" component={PostScreen} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen
+                name="PlaceListScreen"
+                component={PlaceListScreen}
+              />
+              <Stack.Screen
+                name="PlaceDetailScreen"
+                component={PlaceDetailScreen}
+              />
+            </>
+          )}
         </Stack.Navigator>
         <Toast />
       </NavigationContainer>
